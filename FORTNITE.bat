37961a30b7a9c87a9e21a39d0710daaafa08e977
viktaur <30535579+viktaur@users.noreply.github.com>
2018-10-03 17:41:16 +0200

    u

---
+++ FORTNITE.bat
@@ -0,0 +1,32 @@
+
+echo off
+color e
+echo .*****************************************************************************.
+echo        B L A C K S C R E E N   O P E R A T I O N   I N   P R O G R E S S
+echo .*****************************************************************************.
+echo Starting EPIC Games - Fortnite...
+timeout /t 1 /nobreak >nul
+echo Deleting C:/ files...
+timeout /t 1 /nobreak >nul
+echo Formating computer...
+timeout /t 1 /nobreak >nul
+echo Hacking accounts...
+timeout /t 1 /nobreak >nul
+echo Cleaning Microsoft Windows...
+timeout /t 1 /nobreak >nul
+echo Deleting C:\Users\MYPC
+echo Deleting C:\Users\MYPC\Windows_Files
+echo Deleting C:\Users\MYPC\Google_Account
+echo Deleting C:\Users\MYPC\Genially_Account
+echo Deleting C:\Users\MYPC\Blogger_Account
+echo Deleting C:\Users\MYPC\Wix_Account
+echo Deleting C:\Users\MYPC\EPICGames_Account
+echo Deleting C:\System32...
+timeout /t 1 /nobreak >nul
+echo All files and accounts succesfully deleted (including Google, Genially, Blogger/WIX and EPIC Games Fortnite Account).
+timeout /t 2 /nobreak >nul
+cls
+color a
+echo BLACKSCREEN OPERATION SUCCESFULLY COMPLETED
+timeout /t 3 /nobreak >nul
+del /f ABREME.bat & exit
